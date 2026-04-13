3143efae5b6e70472fcdad4f3f70e2be09ddf806
Priyanshu <pradipsinghb601@gmail.com>
2026-04-13 18:50:08 +0530

    Fix DB connection for deployment

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
